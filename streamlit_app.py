bd329b753f6409d918b5e54df1c88089121fb32b
lucas <lucas.bertolini@factored.ai>
2023-01-09 17:10:45 -0300

    Add a menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,10 @@ st.title("My parents new healthy dinner")
 
 x = st.slider("Select a value")
 st.write(x, "squared is", x * x)
+
+
+st.header("Breakfast Menu")
+st.text("Eggs")
+st.text("Toast")
+st.text("Coffee")
+st.text("Milk")
