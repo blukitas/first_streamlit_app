56169337618aca43be746f64bf04907ab18f2d11
lucas <lucas.bertolini@factored.ai>
2023-01-10 12:45:51 -0300

    snowflake connector on streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import requests
+import snowflake.connector
 
 my_fruit_list = pd.read_csv(
     "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
